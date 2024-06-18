e5e364239f473a002c7c9e2e8211982fff94c794
Robert McCreary <113715288+rmccrear@users.noreply.github.com>
2024-06-18 01:34:17 +0000

    fix typo

--- desert.js
+++
@@ -1,7 +0,0 @@
-
-function desertChanger() {
-  myDesertHeader.innerHTML = "Ice Cream";
-  myDesertParagraph.innerHTML = "I like pistachio ice cream."
-}
-
-myIceCreamButton.onclick = desertChanger;
---
+++ dessert.js
@@ -0,0 +1,7 @@
+
+function dessertChanger() {
+  myDessertHeader.innerHTML = "Ice Cream";
+  myDessertParagraph.innerHTML = "I like pistachio ice cream."
+}
+
+myIceCreamButton.onclick = dessertChanger;
